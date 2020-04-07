bcc27ee393a497ea9f6f83a4cdd1de84fe6f768f
18alantom <2.alan.tom@gmail.com>
2020-04-07 17:54:39 +0530

    export required functions

--- identify/__init__.py
+++ identify/__init__.py
@@ -1,1 +1,4 @@
-# from .cam_test import identification
+from .cam_test import identification, detection
+from .get_faces import from_cam, from_images
+from .models import tune_network, check_accuracy
+from .helpers import augment, save_embeddings
--- identify/helpers/__init__.py
+++ identify/helpers/__init__.py
@@ -1,5 +1,5 @@
 from .augment import augment
-# from .save_embeddings import save_embeddings
+from .save_embeddings import save_embeddings
 from .helpers import tensor_to_8b_array, tensor_to_PIL_img, \
     get_face_crops, get_model, create_class_folder, \
     get_index_dict, save_face_crops, get_embeddings
